4acc1459fa3b943102ee90c79ae7befb69f6d4c7
Viktor Lytsus <vlytsus@gmail.com>
2020-10-25 02:12:25 +0100

    Added Speed control HUD menu

---
+++ Assets/HudMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudMenu : MonoBehaviour
+{
+    public GameObject boat;
+    public Text status;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody bootRb = boat.GetComponent<Rigidbody>();
+        status.text = "Speed: " + System.Math.Round(bootRb.velocity.magnitude, 2);
+    }
+}
