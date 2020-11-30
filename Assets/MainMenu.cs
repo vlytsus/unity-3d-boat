70941283d7ba7379537ede3ca96418b0f4a7a4eb
Viktor Lytsus <vlytsus@gmail.com>
2020-12-01 00:15:19 +0100

    Added game caption

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public void play()
-    {
-        Debug.Log("Before load scene");
+public class MainMenu : MonoBehaviour {
+    public void play() {
+        Debug.Log("On MainMenu Play");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void quit() {
+        Debug.Log("On MainMenu Quit");
+        Application.Quit();
+    }
+    
 }
