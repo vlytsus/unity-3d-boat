8a1d3a3bf68be58e93faba9bdb18b383ef35baef
Viktor Lytsus <vlytsus@gmail.com>
2020-11-30 23:52:09 +0100

    Added TextMesh Pro Menu Play button

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void play()
+    {
+        Debug.Log("Before load scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
